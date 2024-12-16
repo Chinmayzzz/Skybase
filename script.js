812e5181f0bee3041d10b1f511d633d398eb231a
iamdeepakdass <iamdeepakdass@gmail.com>
2024-12-17 02:37:41 +0530

    Add the contact details and the routes of the every section from navbar.

--- script.js
+++ script.js
@@ -2,3 +2,13 @@ window.addEventListener("scroll", function () {
   var header = document.querySelector("header");
   header.classList.toggle("sticky", window.scrollY > 0);
 });
+
+document.addEventListener("DOMContentLoaded", function () {
+  const aboutUsLink = document.querySelector('header ul a[href="#about-us"]');
+  const aboutUsSection = document.getElementById("about-us");
+
+  aboutUsLink.addEventListener("click", function (event) {
+    event.preventDefault();
+    aboutUsSection.scrollIntoView({ behavior: "smooth" });
+  });
+});
